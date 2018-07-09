ba55497e5fcc91cacaadab03f461c8b79a416768
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2018-07-10 00:20:57 +0800

    Delete mainmusic.js

--- assets/js/mainmusic.js
+++
@@ -1,4 +0,0 @@
-   $('#musicSwitch').on('click',function(){
-        $(this).hasClass('active')?$(this).removeClass('active'):$(this).addClass('active');
-        if(!$('#skPlayer').find('audio').length){
-        
