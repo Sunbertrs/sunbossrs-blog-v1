a52b733a949233be1407ed36da77d25f6acdb99b
SunbossRS <yaorizhuangzhu@163.com>
2018-08-02 16:16:53 +0800

    util.js upload.sh download.html

--- assets/js/music.js
+++
@@ -1,9 +0,0 @@
-var player = new skPlayer({
-    autoplay: true,
-    listshow: true,
-    mode: 'listloop',
-    music: {
-        type: 'cloud',
-        source: 317921676
-    }
-});
--- upload.sh
+++ upload.sh
@@ -1,36 +1,58 @@
-git config --global user.email "yaorizhuangzhu@163.com"
+﻿git config --global user.email "yaorizhuangzhu@163.com"
 git config --global user.name "SunbossRS"
+
+echo "----------------------------"
+echo "------git上传仓库脚本-------"
+echo "----------------------------"
+echo ""
+echo "Made by SunbossRS"
+echo ""
+echo "This product can help you upload the file easier."
+ 
+clear
+
+echo "运行内容概括"
+echo "1. 检测修改的内容"
+echo "2. 同步仓库＆合并修改"
+echo "3. 输入更改留言"
+echo "4. 更新到仓库"
+echo ""
+echo "Thank you to use this product!"
+echo ""
+echo ""
+echo ""
+echo ""
 
 clear
 
 git status
-echo "请稍后."
-
-
-echo "更新仓库..."
 
 clear
+
 git pull origin master
 
 clear
 
-git status
-
-echo "你的更改已成功。我们将会上传到仓库。"
+echo "你的更改已成功。"
+echo "----------------"
+echo "请输入更改留言。"
 echo ""
-echo "输入更改留言在下方。"
 echo ""
-echo "更改的留言是"
-echo ""
-read -p "->"
+read -p ">"
 
 git add .
 git stage .
 git commit -a -m  "${REPLY}"
 
+clear
+
 echo "更改留言设置成功!"
-echo ""
-echo "查看核实信息!"
+
+clear
+
+echo "已准备就绪。"
+
+echo "查看状态"
 
 clear
 
@@ -41,15 +63,25 @@ git gc
 clear
 
 git push
+git config --global user.email "yaorizhuangzhu@163.com"
+git config --global user.name "SunbossRS"
 
 clear
 
 echo "上传到仓库成功!"
 
-clear 
-
-echo ""
+echo "...."
 git gc
 
 clear
+
+echo "..."
+echo "..."
+echo "..."
+echo "..."
+echo "..."
+echo "..."
+echo "..."
+echo "..."
+echo "close"
 
