9541528042efb5989e6f6aae88b3aad3c9d86cf8
SunbossRS <yaorizhuangzhu@163.com>
2018-08-21 18:57:04 +0800

    更新配置

--- upload.sh
+++ upload.sh
@@ -1,3 +1,21 @@
+**********************************************************************************
+* This script file is a upload git local repository to github.                   *
+*                                                                                *
+* Make sure this file is in '/' and than you can double-click to use this file.  *
+*                                                                                *
+* Make sure your bash terminal resolution must be large.                         *
+*                                                                                *
+* Because something need a large terminal resolution.                            *
+*                                                                                *
+* This script file is created by SunboossRS.                                     *
+*                                                                                *
+* My GitHub: https://github.com/sunbossrs                                        *
+*                                                                                *
+* My Blog: https://sunbossrs.github.io                                           *
+*                                                                                *
+* This is chinese version.                                                       *
+**********************************************************************************
+
 clear
 
 echo "+-----------------------------------------------------+"
@@ -7,13 +25,39 @@ echo "| 这个脚本可以让你上传文件变得容易.                   |"
 echo "|                                                     |"
 echo "|                                                     |"
 echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
 
 git status
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
 
 clear
-
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
 git pull origin master
 
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
 clear
 
 echo "+-----------------------------------------------------+"
@@ -22,15 +66,24 @@ echo "+-----------------------------------------------------+"
 read -p "--> "
 
 echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
 git add .
+echo "|                                                     |"
+echo "|                                                     |"
 echo "|                                                     |"
 git stage .
 echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
 git commit -a -m "${REPLY}"
+echo "|                                                     |"
+echo "|                                                     |"
 echo "|                                                     |"
 
 clear
 
+echo "|                                                     |"
 echo "|                 更改留言设置成功!                   |"
 
 clear
@@ -38,18 +91,26 @@ clear
 echo "|                   已准备就绪。                      |"
 echo "|                                                     |"
 echo "|                       查看状态...                   |"
-
+echo "|                                                     |"
 clear
 
+echo "|                                                     |"
 echo "|                                                     |"
 git status
 echo "|                                                     |"
+echo "|                                                     |"
 git gc
+echo "|                                                     |"
 echo "|                                                     |"
 git push
 echo "|                                                     |"
+echo "|                                                     |"
 clear
 
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
+echo "|                                                     |"
 echo "+-----------------------------------------------------+"
 echo "|                  上传到仓库成功!                    |"
 echo "+-----------------------------------------------------+"
