c41308cc4efd915a35744abb599c7c415874b494
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2018-08-27 21:47:27 +0800

    Update InitJekyllMarkdownArticle.sh

--- InitJekyllMarkdownArticle.sh
+++ InitJekyllMarkdownArticle.sh
@@ -1,50 +1,83 @@
-cd ./_posts
+# This file is default to MY "workspace" folder.
+# If you want,you can delete "cd .." and put it to "/" .
+# https://github.com/sunbossrs
+cd ..
+cd _posts
 clear
 
-echo "欢迎来到init jekyll markdown article服务！"
+echo "P"
+echo "Pl"
+echo "Plz"
+echo "Plz w"
+echo "Plz wa"
+echo "Plz wai"
+echo "Plz wait"
+echo "Plz wait f"
+echo "Plz wait fo"
+echo "Plz wait for"
+echo "Plz wait for g"
+echo "Plz wait for go"
+echo "Plz wait for go t"
+echo "Plz wait for go to"
+echo "Plz wait for go to t"
+echo "Plz wait for go to te"
+echo "Plz wait for go to ter"
+echo "Plz wait for go to term"
+echo "Plz wait for go to term e"
+echo "Plz wait for go to term em"
+echo "Plz wait for go to term emu"
+echo "Plz wait for go to term emul"
+echo "Plz wait for go to term emula"
+echo "Plz wait for go to term emulat"
+echo "Plz wait for go to term emulato"
+echo "Plz wait for go to term emulator"
+echo "Plz wait for go to term emulator ..."
+clear
+echo "minecraft@sunbossrs > "
+echo "minecraft@sunbossrs > init"
+echo "minecraft@sunbossrs > init-jekyll"
+echo "minecraft@sunbossrs > init-jekyll-markdown"
+echo "minecraft@sunbossrs > init-jekyll-markdown-article"
+echo "做成中 ..."
+title "Init Jekyll Markdown Article.service"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "欢迎来到Init Jekyll Markdown Article服务！"
 echo "在这里我们会利用你在git命令行弄出来的本地仓库进行运作."
 echo ""
-echo "确保你是在本地仓库的根目录里运行的这个sh脚本文件~"
 echo ""
 echo ""
-echo ""
-echo "->输入你的文章名称"
+echo "输入你的文章名称"
 echo ""
 echo "Tip: 只要写名字就够了喔~"
 echo ""
 echo ""
 read -p "> "
-
-clear
-
-clear
-
-echo "欢迎来到init jekyll markdown article服务！"
-echo "在这里我们会利用你在git命令行弄出来的本地仓库进行运作."
 echo ""
-echo "确保你是在本地仓库的根目录里运行的这个sh脚本文件~"
+echo ""
+echo "成功定位到[_post]，请等待..."
+echo ""
+echo ""
+echo "请稍等...我们正在创建文章..."
 echo ""
 echo ""
 echo ""
-echo "->请稍等...我们正在创建文章..."
+echo ""
+echo ""
 echo ""
 echo "Created by InitJekyllMarkdownArticle .">"$REPLY.md"
 echo ""
+echo ""
 echo "由InitJekyllMarkdownArticle创建的文件已经创建完成!"
 echo ""
-echo "接下来的就看你的咯~"
-
-clear
-
-echo "欢迎来到init jekyll markdown article服务！"
-echo "在这里我们会利用你在git命令行弄出来的本地仓库进行运作."
 echo ""
-echo "确保你是在本地仓库的根目录里运行的这个sh脚本文件~"
+echo "自动添加文本Created by InitJekyllMarkdownArticle。"
 echo ""
 echo ""
+echo "正在打开编辑器 ..."
 echo ""
-echo "ByeBye"
-
-clear
-
+start .\"$REPLY.md"
 ./"$REPLY.md"
