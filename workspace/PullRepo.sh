06484098036048db1228bf09bef32957cb0b748d
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2018-08-27 21:02:20 +0800

    Update PullRepo.sh

--- workspace/PullRepo.sh
+++ workspace/PullRepo.sh
@@ -1,7 +1,24 @@
+cd ..
+echo "minecraft@sunbossrs >"
+clear
+echo "minecraft@sunbossrs > p"
+clear
+echo "minecraft@sunbossrs > pu"
+clear
+echo "minecraft@sunbossrs > pul"
+clear
+echo "minecraft@sunbossrs > pull"
+clear
+echo "minecraft@sunbossrs > pullr"
+clear
+echo "minecraft@sunbossrs > pullre"
+clear
+echo "minecraft@sunbossrs > pullrep"
+clear
+echo "minecraft@sunbossrs > pullrepo"
+clear
+echo "minecraft@sunbossrs > pullrepo"
 echo "Pulling the repository ..."
 echo ""
 git pull origin master
-
-clear
-
-echo "OK."
+echo "Success to pull repo."
