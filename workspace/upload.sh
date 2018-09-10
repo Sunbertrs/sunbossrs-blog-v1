ef2700b2abab00b23f660254508bbdabd0fe4e8c
SunbossRS <38691765+SunbossRS@users.noreply.github.com>
2018-09-10 22:31:31 +0800

    Update upload.sh

--- workspace/upload.sh
+++ workspace/upload.sh
@@ -19,174 +19,132 @@
 cd ..
 clear
 
-echo "P"
-clear
-echo "Pl"
-clear
-echo "Plz"
-clear
-echo "Plz w"
-clear
-echo "Plz wa"
-clear
-echo "Plz wai"
-clear
-echo "Plz wait"
-clear
-echo "Plz wait f"
-clear
-echo "Plz wait fo"
-clear
-echo "Plz wait for"
-clear
-echo "Plz wait for g"
-clear
-echo "Plz wait for go"
-clear
-echo "Plz wait for go t"
-clear
-echo "Plz wait for go to"
-clear
-echo "Plz wait for go to t"
-clear
-echo "Plz wait for go to te"
-clear
-echo "Plz wait for go to ter"
-clear
-echo "Plz wait for go to term"
-clear
-echo "Plz wait for go to term e"
-clear
-echo "Plz wait for go to term em"
-clear
-echo "Plz wait for go to term emu"
-clear
-echo "Plz wait for go to term emul"
-clear
-echo "Plz wait for go to term emula"
-clear
-echo "Plz wait for go to term emulat"
-clear
-echo "Plz wait for go to term emulato"
-clear
-echo "Plz wait for go to term emulator"
-clear
-echo "Plz wait for go to term emulator ..."
+echo ""
+echo ""
+echo ""
+echo "           Ready ?"
+echo "   This script will start."
+echo ""
+echo ""
 clear
 
-echo "minecraft@sunbossrs > "
+echo ""
+echo ""
+echo ""
+echo "           Ready ?"
+echo "   This script will start."
+echo ""
+echo ">"
 clear
-echo "minecraft@sunbossrs > u"
+
+echo ""
+echo ""
+echo ""
+echo "           Ready ?"
+echo "   This script will start."
+echo ""
+echo ">>>"
 clear
-echo "minecraft@sunbossrs > up"
+
+echo ""
+echo ""
+echo ""
+echo "           Ready ?"
+echo "   This script will start."
+echo ""
+echo ">>>>"
 clear
-echo "minecraft@sunbossrs > upl"
+
+echo ""
+echo ""
+echo ""
+echo "           Ready ?"
+echo "   This script will start."
+echo ""
+echo ">>>>>>>"
 clear
-echo "minecraft@sunbossrs > uplo"
+
+echo ""
+echo ""
+echo ""
+echo "           Ready ?"
+echo "   This script will start."
+echo ""
+echo ">>>>>>>>>>>"
 clear
-echo "minecraft@sunbossrs > uploa"
+
+echo ""
+echo ""
+echo ""
+echo "           Ready ?"
+echo "   This script will start."
+echo ""
+echo ">>>>>>>>>>>>>>>>>"
 clear
-echo "minecraft@sunbossrs > upload"
+
+echo ""
+echo ""
+echo ""
+echo "           Ready ?"
+echo "   This script will start."
+echo ""
+echo ">>>>>>>>>>>>>>>>>>>>>>>"
 clear
-echo "minecraft@sunbossrs > upload."
+
+echo ""
+echo ""
+echo ""
+echo "           Ready ?"
+echo "   This script will start."
+echo ""
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 clear
-echo "minecraft@sunbossrs > upload.g"
+
+echo "startup.git.upload.app"
+echo "Start pull first ..."
 clear
-echo "minecraft@sunbossrs > upload.gi"
-clear
-echo "minecraft@sunbossrs > upload.git"
-clear
-echo "minecraft@sunbossrs > upload.git{"
-clear
-echo "minecraft@sunbossrs > upload.git{ b"
-clear
-echo "minecraft@sunbossrs > upload.git{ bw"
-clear
-echo "minecraft@sunbossrs > upload.git{ bwt"
-clear
-echo "minecraft@sunbossrs > upload.git{ bwtc"
-clear
-echo "minecraft@sunbossrs > upload.git{ bwtck"
-clear
-echo "minecraft@sunbossrs > upload.git{ bwtck }"
-clear
-echo "minecraft@sunbossrs > upload.git{ bwtck }"
-echo "做起中 ..."
-git status
-echo "推送中 ..."
+
 git pull origin master
 echo ""
 echo ""
+echo "推送云端内容完毕。"
+pause
+clear
+
+echo "差不多完成了。"
+echo "我们需要你提供一下更新留言，马上就可以结束。"
+echo "只不过，希望你打出来的字不要有删除修改，否则，在项目里的更新留言可能会出现像 �[D 这样的字符。"
 echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo "推送完毕。"
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo "差不多完成啦！我们需要你提供一下更新留言，马上就可以结束。"
-echo "只不过，希望你打出来的字不要有删除修改，否则，在git项目里的更新留言就会有点难看。可能会出现像�[D这样的字符。"
-echo ""
-read -p ">"
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
+read -p "shell.git.commit >"
+clear
+
+echo "添加文件 ..."
 git add .
+
+echo "stage文件 ..."
 git stage .
+
+echo "commit文件 ..."
 git commit -a -m "${REPLY}"
-echo ""
-echo ""
-echo ""
+
+clear
+
 echo "正在做成 ..."
-echo ""
-echo ""
-echo ""
-echo ""
 git status
+
+echo "gc ..."
 git gc
-echo ""
-echo ""
-echo ""
-echo ""
+
+clear
+
 echo "上传中 ..."
-echo ""
-echo ""
 echo ""
 git push
 echo ""
 echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
+
+clear
+
 echo "感谢使用我们的上传工具!"
 echo ""
 echo " TTTTTTTTTTT   H       H       AA       NN      N   K    K        U        U   "
@@ -195,15 +153,31 @@ echo "      T        HHHHHHHHH     AAAAAA     N   N   N   KK             U      
 echo "      T        H       H    A      A    N    N  N   K  K            U    U     "
 echo "      T        H       H   A        A   N      NN   K    K            UU       "
 echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
+
 echo "请稍等 ...我们正在进行最后一次推送检验..."
 git pull origin master
 
 clear
 
-echo "Bye."
+echo "运行完毕！关掉进程中 ..."
+
+echo ">>"
+clear
+
+echo ">>>>>"
+clear
+
+echo ">>>>>>>>>>>>"
+clear
+
+echo ">>>>>>>>>>>>>>>"
+clear
+
+echo ">>>>>>>>>>>>>>>>>>>>"
+clear
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>"
+clear
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
+clear
