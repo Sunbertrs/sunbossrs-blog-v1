d4083063bcec2d731e77f71986bf67f59f90efad
SunbossRS <yaorizhuangzhu@163.com>
2018-11-15 22:47:05 +0800

    update

--- workspace/InitJekyllMarkdownArticle.sh
+++ workspace/InitJekyllMarkdownArticle.sh
@@ -1,107 +1,44 @@
+# !/bin/bash
 # This file is default to MY "workspace" folder.
 # If you want,you can delete "cd .." and put it to "/" .
 # https://github.com/sunbossrs
+
 cd ..
 cd _posts
 clear
 
-echo "P"
-clear
-echo "Pl"
-clear
-echo "Plz"
-clear
-echo "Plz w"
-clear
-echo "Plz wa"
-clear
-echo "Plz wai"
-clear
-echo "Plz wait"
-clear
-echo "Plz wait f"
-clear
-echo "Plz wait fo"
-clear
-echo "Plz wait for"
-clear
-echo "Plz wait for g"
-clear
-echo "Plz wait for go"
-clear
-echo "Plz wait for go t"
-clear
-echo "Plz wait for go to"
-clear
-echo "Plz wait for go to t"
-clear
-echo "Plz wait for go to te"
-clear
-echo "Plz wait for go to ter"
-clear
-echo "Plz wait for go to term"
-clear
-echo "Plz wait for go to term e"
-clear
-echo "Plz wait for go to term em"
-clear
-echo "Plz wait for go to term emu"
-clear
-echo "Plz wait for go to term emul"
-clear
-echo "Plz wait for go to term emula"
-clear
-echo "Plz wait for go to term emulat"
-clear
-echo "Plz wait for go to term emulato"
-clear
-echo "Plz wait for go to term emulator"
-clear
-echo "Plz wait for go to term emulator ..."
-clear
-
-echo "minecraft@sunbossrs > "
-clear
-echo "minecraft@sunbossrs > init"
-clear
-echo "minecraft@sunbossrs > init-jekyll"
-clear
-echo "minecraft@sunbossrs > init-jekyll-markdown"
-clear
-echo "minecraft@sunbossrs > init-jekyll-markdown-article"
-echo "做成中 ..."
-title "Init Jekyll Markdown Article.service"
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
 echo "欢迎来到Init Jekyll Markdown Article服务！"
-echo "在这里我们会利用你在git命令行弄出来的本地仓库进行运作."
+echo "在这里我们会利用你弄出来的本地网页仓库进行运作."
 echo ""
 echo ""
 echo ""
 echo "输入你的文章名称"
 echo ""
-echo "Tip: 只要写名字就够了喔~"
+echo "只要写名字！"
 echo ""
 echo ""
-read -p "> "
-echo ""
-echo ""
+read -p "你的输入>"
+
+clear
+
 echo "成功定位到[_post]，请等待..."
 echo ""
 echo ""
 echo "请稍等...我们正在创建文章..."
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo "Created by InitJekyllMarkdownArticle .">"$REPLY.md"
-echo ""
-echo ""
+
+clear
+
+echo "---">"$REPLY.md"
+echo "layout: post">"$REPLY.md"
+echo "title: '' ">"$REPLY.md"
+echo "subtitle: '' ">"$REPLY.md"
+echo "tags: '' '' ">"$REPLY.md"
+echo "category: '' ">"$REPLY.md"
+echo "keyword: '' ">"$REPLY.md"
+echo "title: '' ">"$REPLY.md"
+echo "cover: '' ">"$REPLY.md"
+
+echo "">"$REPLY.md"
 echo "由InitJekyllMarkdownArticle创建的文件已经创建完成!"
 echo ""
 echo ""
---
+++ workspace/qwerty.sh
@@ -0,0 +1,2 @@
+read -p "sdvs"
+exit
